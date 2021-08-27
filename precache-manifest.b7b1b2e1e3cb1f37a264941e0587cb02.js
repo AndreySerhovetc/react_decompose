a5ed3ed3958d258337a1d5a73cfc327170eb37d6
Andrii Serhovetc <serhovetc88@gmail.com>
2021-08-27 16:05:01 +0300

    Updates

---
+++ precache-manifest.b7b1b2e1e3cb1f37a264941e0587cb02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e2d539f96a9218986dc84e2dc601766",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3f03df8ec0477e6ef7f9",
+    "url": "./static/css/main.7bc7788b.chunk.css"
+  },
+  {
+    "revision": "a3fa78d2a47eeb043805",
+    "url": "./static/js/2.ac9e10eb.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.ac9e10eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f03df8ec0477e6ef7f9",
+    "url": "./static/js/main.efb93a76.chunk.js"
+  },
+  {
+    "revision": "8b3c3b96386eee1ea33a",
+    "url": "./static/js/runtime-main.b672bc02.js"
+  }
+]);
